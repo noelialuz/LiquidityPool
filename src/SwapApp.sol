06de1cfe10dea40707a0586b9dd61153fcb22c5c
“noelialuz” <noelialuzfernandez@gmail.com>
2026-07-08 04:14:27 -0300

    update and add new functions

--- src/SwapApp.sol
+++ src/SwapApp.sol
@@ -15,19 +15,29 @@ contract SwapApp is ReentrancyGuard, Ownable {
     address public immutable UniswapFactoryAddress;
     address public immutable USDT;
     address public immutable DAI;
+    address public immutable LP_TOKEN;
+
+    error InsufficientBalance(uint256 requested, uint256 available);
+
+    mapping(address => uint256) public userLpBalances;
 
     event SwapTokens(address indexed tokenIn, address indexed tokenOut, uint256 amountIn, uint256 amountOut);
-    event AddLiquidity(address indexed token0, address indexed token1, uint256 lpTokenAmount);
+    event AddLiquidity(address indexed user, uint256 lpTokenAmount);
+    event RemoveLiquidity(address indexed user, uint256 lpTokenAmount);
 
     constructor(address _router, address _factory, address _usdt, address _dai) Ownable(msg.sender) {
-        require(_router != address(0) && _factory != address(0) && _usdt != address(0) && _dai != address(0), "Invalid address");
         V2Router02Address = _router;
         UniswapFactoryAddress = _factory;
         USDT = _usdt;
         DAI = _dai;
+        LP_TOKEN = IFactory(_factory).getPair(_usdt, _dai);
     }
 
     function swapTokens(uint256 amountIn_, uint256 amountOutMin_, address[] calldata path_, address to_, uint256 deadline_) public nonReentrant returns (uint256) {
+        uint256 balance = IERC20(path_[0]).balanceOf(msg.sender);
+        if (balance < amountIn_) {
+            revert InsufficientBalance(amountIn_, balance);
+        }
         IERC20(path_[0]).safeTransferFrom(msg.sender, address(this), amountIn_);
         IERC20(path_[0]).forceApprove(V2Router02Address, amountIn_);
         
@@ -42,21 +52,31 @@ contract SwapApp is ReentrancyGuard, Ownable {
         IERC20(USDT).safeTransferFrom(msg.sender, address(this), amountIn_);
         
         uint256 swappedAmount = swapTokens(amountSwap_, amountOutMin_, path_, address(this), deadline_);
-
-        uint256 usdtRemaining;
-        unchecked { usdtRemaining = amountIn_ - amountSwap_; }
+        uint256 usdtRemaining = amountIn_ - amountSwap_;
         
         IERC20(USDT).forceApprove(V2Router02Address, usdtRemaining);
         IERC20(DAI).forceApprove(V2Router02Address, swappedAmount);
 
-        (,,uint256 lpTokenAmount) = IV2Router02(V2Router02Address).addLiquidity(USDT, DAI, usdtRemaining, swappedAmount, amountAMin_, amountBMin_, msg.sender, deadline_);
+        (,,uint256 lpTokenAmount) = IV2Router02(V2Router02Address).addLiquidity(USDT, DAI, usdtRemaining, swappedAmount, amountAMin_, amountBMin_, address(this), deadline_);
 
-        uint256 usdtLeft = IERC20(USDT).balanceOf(address(this));
-        if (usdtLeft > 0) IERC20(USDT).safeTransfer(msg.sender, usdtLeft);
-        uint256 daiLeft = IERC20(DAI).balanceOf(address(this));
-        if (daiLeft > 0) IERC20(DAI).safeTransfer(msg.sender, daiLeft);
+        userLpBalances[msg.sender] += lpTokenAmount;
 
-        emit AddLiquidity(USDT, DAI, lpTokenAmount);
+        emit AddLiquidity(msg.sender, lpTokenAmount);
+    }
+
+    function removeLiquidity(uint256 liquidityAmount_, uint256 amountAMin_, uint256 amountBMin_, address to_, uint256 deadline_) external nonReentrant {
+ 
+        uint256 userBalance = userLpBalances[msg.sender];
+        if (userBalance < liquidityAmount_) {
+            revert InsufficientBalance(liquidityAmount_, userBalance);
+        }
+
+        userLpBalances[msg.sender] -= liquidityAmount_;
+
+        IERC20(LP_TOKEN).forceApprove(V2Router02Address, liquidityAmount_);
+        IV2Router02(V2Router02Address).removeLiquidity(USDT, DAI, liquidityAmount_, amountAMin_, amountBMin_, to_, deadline_);
+
+        emit RemoveLiquidity(msg.sender, liquidityAmount_);
     }
 
     function rescueERC20(address token_, uint256 amount_) external onlyOwner {
--- test/SwapApp.t.sol
+++ test/SwapApp.t.sol
@@ -4,24 +4,96 @@ pragma solidity 0.8.35;
 import "forge-std/Test.sol";
 import "../src/SwapApp.sol";
 
-contract SwappAppTest is Test {
+contract SwapAppTest is Test {
     SwapApp app;
-    address router = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
-    address factory = 0xf1D7CC64Fb4452F05c498126312eBE29f30Fbcf9;
-    address user = 0xB45323118e29e3C33c4a906dD8ce9d9CF443D380;
-    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
-    address DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
+    address uniswapV2SwappRouterAddress = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address uniswapv2FactoryAddress = 0xf1D7CC64Fb4452F05c498126312eBE29f30Fbcf9;
+    address attacker = address(0x2);
+    address user = 0xB45323118e29e3C33c4a906dD8ce9d9CF443D380; // Address with USDT in Arbitrum Mainnet
+    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9; // USDT address in Arbitrum Mainnet
+    address DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1; // DAI address in Arbitrum Mainnet
 
+    
     function setUp() public {
-        app = new SwapApp(router, factory, USDT, DAI);
+        app = new SwapApp(uniswapV2SwappRouterAddress, uniswapv2FactoryAddress, USDT, DAI);
     }
 
-    function testRescueFunction() public {
+    function test_Rescue_Unauthorized() public {
+        vm.prank(attacker);
+        vm.expectRevert();
+        app.rescueERC20(USDT, 100);
+    }
+
+    function test_RemoveLiquidity_NoBalance() public {
         vm.prank(user);
-        IERC20(USDT).transfer(address(app), 1e6);
+        vm.expectRevert(abi.encodeWithSelector(SwapApp.InsufficientBalance.selector, 1e18, 0));
+        app.removeLiquidity(1e18, 0, 0, user, block.timestamp + 100);
+    }
+
+    function testFuzz_RemoveLiquidity_AccessControl(uint256 amount) public {
+        vm.assume(amount > 0);
+        vm.prank(attacker);
+        vm.expectRevert(abi.encodeWithSelector(SwapApp.InsufficientBalance.selector, amount, 0));
+        app.removeLiquidity(amount, 0, 0, attacker, block.timestamp + 100);
+    }
+
+    function test_SwapTokens_InsufficientFunds() public {
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
         
-        uint256 balBefore = IERC20(USDT).balanceOf(address(this));
-        app.rescueERC20(USDT, 1e6);
-        assertGt(IERC20(USDT).balanceOf(address(this)), balBefore);
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(SwapApp.InsufficientBalance.selector, 1e6, 0));
+        app.swapTokens(1e6, 0, path, user, block.timestamp + 100);
+    }
+
+    function testFuzz_AddLiquidity_Logic(uint256 amount) public {
+        vm.assume(amount > 1e6 && amount < 1e18);
+        deal(USDT, user, amount);
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
+
+        vm.startPrank(user);
+        IERC20(USDT).approve(address(app), amount);
+        app.addLiquidity(amount, amount / 4, 0, path, 0, 0, block.timestamp + 100);
+        
+        assertGt(app.userLpBalances(user), 0);
+        vm.stopPrank();
+    }
+
+    function test_RemoveLiquidity_Success() public {
+        uint256 amount = 1000e6;
+        deal(USDT, user, amount);
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
+
+        vm.startPrank(user);
+        IERC20(USDT).approve(address(app), amount);
+        app.addLiquidity(amount, 100e6, 0, path, 0, 0, block.timestamp + 100);
+        
+        uint256 bal = app.userLpBalances(user);
+        app.removeLiquidity(bal, 0, 0, user, block.timestamp + 100);
+        
+        assertEq(app.userLpBalances(user), 0);
+        vm.stopPrank();
+    }
+    
+    function test_RemoveLiquidity_UnauthorizedAccess() public {
+        uint256 amount = 1000e6;
+        deal(USDT, user, amount);
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
+
+        vm.startPrank(user);
+        IERC20(USDT).approve(address(app), amount);
+        app.addLiquidity(amount, 100e6, 0, path, 0, 0, block.timestamp + 100);
+        vm.stopPrank();
+
+        vm.prank(attacker);
+        vm.expectRevert(abi.encodeWithSelector(SwapApp.InsufficientBalance.selector, 1e18, 0));
+        app.removeLiquidity(1e18, 0, 0, attacker, block.timestamp + 100);
     }
 }
